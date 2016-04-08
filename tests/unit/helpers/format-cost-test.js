f5cbec5c285b5b793a104a480b2f58c330c8c36e
Lauryn Davis <lryndavis@gmail.com>
2016-04-08 09:10:28 -0700

    generate custom helper for formatting currency

---
+++ tests/unit/helpers/format-cost-test.js
@@ -0,0 +1,10 @@
+import { formatCost } from 'question/helpers/format-cost';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format cost');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatCost([42]);
+  assert.ok(result);
+});
